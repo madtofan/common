51a0c344631d5a4e3115b3a08f722880a061e1c8
Ahmad Zainul Abidin <45701818+madtofan@users.noreply.github.com>
2023-01-28 19:17:38 +0800

    [PR-2201] Split Api-Gateway with common (#1)
    * [PR-2201] Split Api-Gateway with common
    
    * review fixes

---
+++ src/repository/connection_pool.rs
@@ -0,0 +1,18 @@
+use anyhow::Context;
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Pool, Postgres};
+
+pub type ServiceConnectionPool = Pool<Postgres>;
+pub struct ServiceConnectionManager;
+
+impl ServiceConnectionManager {
+    pub async fn new_pool(connection_string: &str) -> anyhow::Result<ServiceConnectionPool> {
+        let pool = PgPoolOptions::new()
+            .max_connections(5)
+            .connect(connection_string)
+            .await
+            .context("error while initializing the database connection pool")?;
+
+        Ok(pool)
+    }
+}
