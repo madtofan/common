b8caf61fe11878fc3281493072cbe6f6e5033f01
Ahmad Zainul Abidin <45701818+madtofan@users.noreply.github.com>
2023-06-19 21:23:55 +0800

    [PR-3293] Generated code in common lib (#9)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+pub use self::gen::*;
 pub mod errors;
 pub mod repository;
 pub mod token;
+
+mod gen {
+    pub mod email {
+        tonic::include_proto!("email");
+    }
+
+    pub mod templating {
+        tonic::include_proto!("templating");
+    }
+
+    pub mod user {
+        tonic::include_proto!("user");
+    }
+}
