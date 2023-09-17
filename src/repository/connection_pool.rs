e14d3de76c772bd0e9ab568d42c98bc999d48993
Ahmad Zainul Abidin <ahmadclab@gmail.com>
2023-09-17 17:41:13 +0800

    Added timeout for DB connection

--- src/repository/connection_pool.rs
+++ src/repository/connection_pool.rs
@@ -9,6 +9,7 @@ impl ServiceConnectionManager {
     pub async fn new_pool(connection_string: &str) -> anyhow::Result<ServiceConnectionPool> {
         let pool = PgPoolOptions::new()
             .max_connections(5)
+            .acquire_timeout(std::time::Duration::from_secs(60))
             .connect(connection_string)
             .await
             .context("error while initializing the database connection pool")?;
